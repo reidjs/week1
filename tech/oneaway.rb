1fe22c1ae68cd5d30e5a8521b9f2381814669764
reid <reidsherman@gmail.com>
2017-10-01 09:10:05 -0700

    uses procs in oneaway.rb

--- tech/oneaway.rb
+++ tech/oneaway.rb
@@ -28,12 +28,6 @@ def iterate(str, prc)
   end
 end
 
-rm = Proc.new do |c|
-  c = ""
-end
-
-
-
 
 #create a method that will take every index of the string and apply the relevent
 #operation, like remove, insert, etc.
@@ -47,14 +41,65 @@ def remove_chars(string)
 
 end
 
-def loop_through_indices(string, &prc)
-
+def loop_through_indices(string, prc)
+  i = 0
+  while i < string.length
+    prc.call(string, i)
+    i += 1
+  end
+  string
 end
 
 
 
-x = "hello"
-p remove(x, 0)
-p insert(x, 3, "B")
-p replace(x, 3, "B")
-p x
+def each_letter
+  letters = ('a'..'z').to_a
+  letters.each do |l|
+    yield l
+  end
+end
+
+def check_string(string, desired = "pale")
+  string == desired
+end
+
+replace = Proc.new do |str, idx|
+  orig_letter = str[idx]
+  each_letter do |l|
+    orig_letter != l ? str[idx] = l : next
+    break if check_string(str)
+  end
+  check_string(str) ? break : str[idx] = orig_letter
+end
+string = "pable"
+replace.call(string, 2)
+p string
+
+
+#
+# class LinkedListAlphabet
+#   attr_accessor :nodes
+#   def initialize
+#     nodes = []
+#     letters = ('a'..'z').to_a
+#     letters.each do |l|
+#
+#     end
+#   end
+#
+#   def node(val, next_node)
+#     next_node = nil
+#     value = val
+#   end
+# end
+
+
+
+# p loop_through_indices("hello", replace)
+
+#
+# x = "hello"
+# p remove(x, 0)
+# p insert(x, 3, "B")
+# p replace(x, 3, "B")
+# p x
