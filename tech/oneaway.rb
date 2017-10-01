deab82cdccb497af8021cf419714074034584567
reid <reidsherman@gmail.com>
2017-10-01 10:10:28 -0700

    oneaway appears to function properly, should write test cases now

--- tech/oneaway.rb
+++ tech/oneaway.rb
@@ -69,11 +69,73 @@ replace = Proc.new do |str, idx|
     orig_letter != l ? str[idx] = l : next
     break if check_string(str)
   end
-  check_string(str) ? break : str[idx] = orig_letter
+  check_string(str) ? str : str[idx] = orig_letter
 end
-string = "pable"
-replace.call(string, 2)
-p string
+
+remove = Proc.new do |str, idx|
+  orig_string = str.dup
+  str[idx] = ""
+  check_string(str) ? str : orig_string
+end
+
+
+# string = "paze"
+# # replace.call(string, 2)
+# string = remove.call(string, 1)
+# p string
+
+#TO DO: Now try to make these methods so they ONLY do the logic
+
+
+
+# test_remove = "pabble"
+# p remove2.call(test_remove.dup, 2)
+#
+#
+#
+# test_replace = "paze"
+#
+# each_letter do |l|
+#   break if replace2.call(test_replace.dup, 2, l)
+# end
+#
+#
+#
+# test_insert = "pae"
+# each_letter do |l|
+#   break if insert2.call(test_insert.dup, 1, l)
+# end
+
+
+def oneaway(string, desired = "pale")
+  remove = Proc.new do |str, idx|
+    str[idx] = ""
+    str
+  end
+
+  replace = Proc.new do |str, idx, l|
+    str[idx] = l if idx < str.length
+    str
+  end
+
+  insert = Proc.new do |str, idx, l|
+    idx <= str.length ? str.insert(idx, l) : str << l
+    str
+  end
+
+  i = 0
+  while i <= string.length
+    return true if check_string(remove.call(string.dup, i))
+    each_letter do |l|
+      return true if check_string(replace.call(string.dup, i, l), desired)
+      return true if check_string(insert.call(string.dup, i, l), desired)
+    end
+    i += 1
+  end
+  false
+end
+
+p oneaway("palezd", "pale")
 
 
 #
