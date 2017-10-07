c06eada575a38bd9dfaf073cd2c53ae9f0dd0880
reid <reidsherman@gmail.com>
2017-10-07 14:11:33 -0700

    Implement my_include to array class

--- technical interview qs/my_include.rb
+++ technical interview qs/my_include.rb
@@ -1,2 +1,10 @@
-#add my include class to the Array class 
+#add my include class to the Array class
 class Array
+  def my_include?(val)
+    self.each do |el|
+      return true if el == val
+    end
+    false
+  end
+end
+p [4,2,5,2,1].my_include?(50)
